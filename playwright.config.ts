c5bec139deb67d54f910cbc8e4e879806c09e2c8
chandan kumar <chk255@gmail.com>
2026-03-14 00:34:34 +0530

    added headless and headed automatic capability

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
     screenshot:'only-on-failure',
     video:'retain-on-failure',
     trace: 'retain-on-failure',
-    headless:false,
+    headless: !!process.env.CI,
     
     
   },
