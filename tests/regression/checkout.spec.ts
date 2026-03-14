d3f4514ea782d04d4bc4c85c92d8a6a1f737f012
chandankumar-qa-ai <chandankumar-qa-ai@users.noreply.github.com>
2026-03-14 09:33:34 +0000

    deploy: c7ff5f363a7547c75bbd11836343bded3be21ba5

--- tests/regression/checkout.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "../../fixtures/pageFixture";
-
-test("@regression Checkout the Product", async ({ page, checkoutPage,overviewPage }) => {
-  await page.goto("/checkout-step-one.html");
-  expect(await checkoutPage.CheckoutPageTitle()).toBe("Checkout: Your Information",);
-  await checkoutPage.Continue("chandan", "kumar", "201305");
-  expect(await overviewPage.OverviewPageTitle()).toBe("Checkout: Overview");
-});
