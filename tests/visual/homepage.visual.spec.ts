b8996e5d282354bfbbc3513ca386a5f6be002793
chandan kumar <chk255@gmail.com>
2026-03-14 15:20:42 +0530

    test deployement9

---
+++ tests/visual/homepage.visual.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "../../fixtures/pageFixture"
+import { LoginPage } from "../../pages/LoginPage";
+import { InventoryPage } from "../../pages/InventoryPage";
+import dotenv from 'dotenv'
+import { ENV } from "../../config/env"
+
+
+test("@smoke Login with Valid Credential2", async ({ loginPage,inventoryPage }) => {
+  await loginPage.launchApplication(ENV.BASE_URL!);
+  await loginPage.login(ENV.USERNAME!, ENV.PASSWORD!);
+  expect(await inventoryPage.CurrentURL()).toContain("inventory");
+  expect(await inventoryPage.InventoryPageTitle()).toBe("Products");
+});
