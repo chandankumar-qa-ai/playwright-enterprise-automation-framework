6fac6d394f2a0f0f659ce669e3bdfe49d2731289
chandan kumar <chk255@gmail.com>
2026-03-16 15:44:24 +0530

    updated YML File and Visual test

--- tests/visual/homepage.visual.spec.ts
+++ tests/visual/homepage.visual.spec.ts
@@ -1,19 +1,11 @@
-import { test, expect } from "../../fixtures/pageFixture"
+import { test, expect } from "../../fixtures/pageFixture";
 import { InventoryPage } from "../../pages/InventoryPage";
-import dotenv from 'dotenv'
-import { ENV } from "../../config/env"
 
-const validUser = { username: 'standard_user', password: 'secret_sauce' };
 
-test('visual regression: inventory page layout', async ({ page,inventoryPage }) => {
+test("visual regression: inventory page layout", async ({ page, inventoryPage,}) => {
   await page.goto("/inventory.html");
+  await page.waitForLoadState('networkidle');
+  await page.waitForTimeout(500);
   expect(await inventoryPage.CurrentURL()).toContain("inventory");
-  await test.expect(page).toHaveScreenshot('inventory-page.png', { fullPage: true });
+  await test.expect(page).toHaveScreenshot("inventory-page.png", {  animations: 'disabled',maxDiffPixelRatio: 0.01,fullPage: true });
 });
-
-
-
-
-
-
-
