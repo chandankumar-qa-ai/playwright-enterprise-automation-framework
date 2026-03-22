d96c107eb3a717db5034f152e78ad84107bf0142
chandan kumar <chk255@gmail.com>
2026-03-22 13:37:36 +0530

    run

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,9 @@ export default defineConfig({
   workers: process.env.CI ? 2 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'],['allure-playwright']],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   globalSetup: './tests/setup/auth.setup.ts',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
